4dc0b6299b01dd89242b8494d0e72c9c5de6674c
lyndonpett <lyndonpett98@gmail.com>
2021-09-13 15:18:16 -0500

    Fixed maybe a edgecase?

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 
 	if (argc != 2) /*checks if correct amount of argcs*/
 	{
-		dprintf(STDERR_FILENO, "Error: monty file\n");
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	fd = fopen(argv[1], "r"); /*opens and reads file*/
