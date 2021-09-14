a9274628792949f28cb035da731d6a7e1516970f
lyndonpett <lyndonpett98@gmail.com>
2021-09-14 11:52:26 -0500

    Added opcode addOP

--- monty2.c
+++ monty2.c
@@ -11,10 +11,29 @@
  */
 void addOP(stack_t **stack, unsigned int line_num, char *opcode, FILE *fd)
 {
-	stack = stack;
-	line_num = line_num;
-	opcode = opcode;
-	fd = fd;
+	stack_t *tmp, *cog;
+
+	if (*stack == NULL ||
+	    (((*stack)->prev == NULL) && (*stack)->next == NULL))
+	{
+		dprintf(STDERR_FILENO, "L%d: can't swap, stack too short\n",
+			line_num);
+		freeList(*stack);
+		free(opcode);
+		fclose(fd);
+		exit(EXIT_FAILURE);
+	}
+	for (tmp = *stack; tmp->prev; tmp = tmp->prev)
+	{
+	}
+	cog = tmp->next;
+	if (tmp == *stack)
+	{
+		*stack = cog;
+	}
+	cog->n = tmp->n + cog->n;
+	cog->prev = NULL;
+	free(tmp);
 }
 /**
  * nopOP - performs add op
