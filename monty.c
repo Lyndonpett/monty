876cce1c65905554001f6dbfdfeae23703189b2d
lyndonpett <lyndonpett98@gmail.com>
2021-09-14 11:35:44 -0500

    Added opcode swap

--- monty.c
+++ monty.c
@@ -141,8 +141,22 @@ void popOP(stack_t **stack, unsigned int line_num, char *opcode, FILE *fd)
  */
 void swapOP(stack_t **stack, unsigned int line_num, char *opcode, FILE *fd)
 {
-	stack = stack;
-	line_num = line_num;
-	opcode = opcode;
-	fd = fd;
+	stack_t *tmp;
+
+	tmp = *stack;
+	if (tmp == NULL || (tmp->next == NULL && tmp->prev == NULL))
+	{
+		dprintf(STDERR_FILENO, "L%d: can't swap, stack too short\n",
+			line_num);
+		free(tmp);
+		freeList(*stack);
+		free(opcode);
+		fclose(fd);
+		exit(EXIT_FAILURE);
+	}
+	tmp->prev = tmp->next;
+	tmp->next->prev = NULL;
+	tmp->next = tmp->next->next;
+	tmp->prev->next = tmp;
+	(*stack) = tmp->prev;
 }
